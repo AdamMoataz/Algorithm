df6ee87e1ed834dac7939b657b1c34eacb074e6a
Adam Moataz <adam.moataz.hindawi2014@gmail.com>
2024-06-06 20:58:21 +0300

    i did he homework.

---
+++ homework .py
@@ -0,0 +1,8 @@
+"""
+write the algorithm to subtract two numbers ?
+eg 80 - 10
+
+1. Create a varible for number 1
+2. Create a varible for number 2
+Subtract variable number 1 by variable number 2
+"""
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 """
 write the algorithm to add two numbers ?
 eg 80 + 10
@@ -5,3 +6,8 @@ eg 80 + 10
 1. Create a varible for number 1
 2. Create a varible for number 2
 """
+# declaring the variables
+number_1 = 9
+number_2 = 7
+# adding the variable
+print ( number_1 + number_2 )
